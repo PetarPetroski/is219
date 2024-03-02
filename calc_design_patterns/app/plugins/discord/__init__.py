e6f80d6348376249e60d44a9c63b40a7bb090899
Petar Petroski <petar.petroski2003@gmail.com>
2024-03-01 21:09:06 -0500

    commit

--- calc_design_patterns/app/plugins/discord/__init__.py
+++ calc_design_patterns/app/plugins/discord/__init__.py
@@ -3,6 +3,6 @@ from app.commands import Command
 import logging
 
 class DiscordCommand(Command):
-    def execute(self):
+    def execute(self, *args):
         logging.info("I WIll send something to discord")
         print(f'I WIll send something to discord')
